10e3ad407c72a261dff86f0dd07e80cb20ef91e5
Alexandre Duchenne <alexandre.duchenne@lacombedulionvert.fr>
2021-10-19 17:28:54 +0200

    refacto

--- src/main/java/fr/lacombedulionvert/fraction/Fraction.java
+++ src/main/java/fr/lacombedulionvert/fraction/Fraction.java
@@ -1,16 +1,14 @@
 package fr.lacombedulionvert.fraction;
 
-public record Fraction(int value, int denominator) {
+public record Fraction(int numerator, int denominator) {
 
   public Fraction(int numerator) {
     this(numerator, 1);
   }
 
   public Fraction add(Fraction fraction) {
-    if(denominator!= fraction.denominator){
-      return new Fraction(value*fraction.denominator+fraction.value*denominator,
+    return new Fraction(numerator *fraction.denominator+fraction.numerator *denominator,
               denominator*fraction.denominator);
-    }
-    return new Fraction(value + fraction.value);
+
   }
 }
