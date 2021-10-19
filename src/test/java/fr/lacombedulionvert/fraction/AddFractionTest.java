c7776842c215a9fe8b212349cdf75035ea550a36
Alexandre Duchenne <alexandre.duchenne@lacombedulionvert.fr>
2021-10-19 15:08:07 +0200

    Parameterized test

--- src/test/java/fr/lacombedulionvert/fraction/AddFractionTest.java
+++ src/test/java/fr/lacombedulionvert/fraction/AddFractionTest.java
@@ -1,6 +1,8 @@
 package fr.lacombedulionvert.fraction;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +42,19 @@ class AddFractionTest {
     Fraction result = one.add(one);
 
     assertThat(result).isEqualTo(new Fraction(2));
+  }
+
+  @ParameterizedTest
+  @CsvSource(
+      value = {
+        "0,0,0", "0,1,1", "1,0,1",
+      })
+  void test(int givenLeft, int givenRight, int expected) {
+    Fraction left = new Fraction(givenLeft);
+    Fraction right = new Fraction(givenRight);
+
+    Fraction result = left.add(right);
+
+    assertThat(result).isEqualTo(new Fraction(expected));
   }
 }
