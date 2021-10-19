5ce68d2a56e05c7001fc577bc0e00c16babe5b7a
Alexandre Duchenne <alexandre.duchenne@lacombedulionvert.fr>
2021-10-19 14:57:55 +0200

    suppression du if

--- src/main/java/fr/lacombedulionvert/fraction/Fraction.java
+++ src/main/java/fr/lacombedulionvert/fraction/Fraction.java
@@ -3,9 +3,6 @@ package fr.lacombedulionvert.fraction;
 public record Fraction(int value) {
 
   public Fraction add(Fraction fraction) {
-    if (value == 0) {
-      return fraction;
-    }
     return new Fraction(value + fraction.value);
   }
 }
