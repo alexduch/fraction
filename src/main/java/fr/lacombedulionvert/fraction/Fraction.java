6a3ecef2a88868344d9a161c6894223a7510d452
Alexandre Duchenne <alexandre.duchenne@lacombedulionvert.fr>
2021-10-19 14:46:15 +0200

    one plus one

--- src/main/java/fr/lacombedulionvert/fraction/Fraction.java
+++ src/main/java/fr/lacombedulionvert/fraction/Fraction.java
@@ -1,10 +1,11 @@
 package fr.lacombedulionvert.fraction;
 
-public class Fraction {
+public record Fraction(int value) {
 
-  public Fraction(int value) {}
-
-  public Fraction add(Fraction zero) {
-    return zero;
+  public Fraction add(Fraction fraction) {
+    if (value == 0) {
+      return fraction;
+    }
+    return new Fraction(value + fraction.value);
   }
 }
--- src/test/java/fr/lacombedulionvert/fraction/AddFractionTest.java
+++ src/test/java/fr/lacombedulionvert/fraction/AddFractionTest.java
@@ -14,4 +14,27 @@ class AddFractionTest {
 
     assertThat(result).isEqualTo(zero);
   }
+
+  @Test
+  void shouldReturnOneWhenAddOneToZero() {
+    Fraction one = new Fraction(1);
+
+    Fraction result = new Fraction(0).add(one);
+
+    assertThat(result).isEqualTo(one);
+  }
+
+  @Test
+  void shouldReturnOneWhenAddZeroToOne() {
+    Fraction one = new Fraction(1);
+
+    Fraction zero = new Fraction(0);
+
+    assertThat(one.add(zero)).isEqualTo(one);
+  }
+
+  @Test
+  void shouldReturnTwoWhenAddOneToOne() {
+    assertThat(new Fraction(1).add(new Fraction(1))).isEqualTo(new Fraction(2));
+  }
 }
